3e56801d14461ff8b5f6849237c3bfc0721d6637
Neha Rahel <neharam1@umbc.edu>
2024-12-01 21:45:53 -0500

    Update Login.java
    This file is for the user to login and to provide required information to help make their workout routine.

--- src/Login.java
+++ src/Login.java
@@ -12,7 +12,7 @@ public class Login {
             System.out.println("Username exists!");
         } else {
             storage.addUser(new User(username, password));
-            System.out.println("User registered");
+            System.out.println("User registered successfully.");
         }
     }
 
@@ -20,8 +20,6 @@ public class Login {
         if (storage.validCredentials(username, password)) {
             System.out.println("Login successful!");
             User user = storage.getUser(username);
-
-            runBmiCalculator(user); // Run BMI calculator after login
         } else {
             System.out.println("Invalid username or password.");
         }
@@ -29,21 +27,19 @@ public class Login {
 
     public void runBmiCalculator(User user) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your weight in kilograms: ");
+        System.out.print("Enter your weight in pounds: ");
         double weight = scanner.nextDouble();
-        System.out.println(weight);
-
-        System.out.print("Enter your height in meters: ");
+        System.out.print("Enter your height in cm: ");
         double height = scanner.nextDouble();
-        System.out.println(height);
 
-        double bmi = weight / (height * height);
-        user.setBmi(bmi); // Save BMI to user
+        // BMI formula (weight in lbs, height in cm)
+        double heightInMeters = height / 100;
+        double bmi = (weight / (heightInMeters * heightInMeters)) * 703;
+
+        user.setBmi(bmi);
 
         System.out.printf("Your BMI is: %.2f%n", bmi);
 
-        // Display BMI
         if (bmi < 18.5) {
             System.out.println("You are underweight.");
         } else if (bmi >= 18.5 && bmi < 24.9) {
@@ -53,31 +49,5 @@ public class Login {
         } else {
             System.out.println("You are obese.");
         }
-
-        System.out.println("BMI has been recorded.");
-
-        // Prompt user for fitness goal
-        System.out.println("What is your fitness goal?");
-        System.out.println("1. Gain weight");
-        System.out.println("2. Lose weight");
-        System.out.println("3. Gain muscle");
-        int goalChoice = scanner.nextInt();
-        scanner.nextLine(); // Consume newline
-
-        switch (goalChoice) {
-            case 1:
-                user.setGoal("Gain weight");
-                break;
-            case 2:
-                user.setGoal("Lose weight");
-                break;
-            case 3:
-                user.setGoal("Gain muscle");
-                break;
-            default:
-                System.out.println("Invalid choice, no goal set.");
-        }
-        System.out.println("Your goal is: " + user.getGoal());
-        System.out.println("Goal has been recorded.");
     }
 }
